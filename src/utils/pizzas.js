ef8b14211b63587db29537d5eb90c5afe58bb819
JanisCatalina10 <janiscatalinaa@gmail.com>
2024-10-31 20:19:58 -0300

    3 ultimas pizzas agregadas al array

--- src/utils/pizzas.js
+++ src/utils/pizzas.js
@@ -19,5 +19,26 @@ export const pizzas = [
       price: 6950,
       ingredients: ["mozzarella", "pepperoni", "orégano"],
       img: "https://firebasestorage.googleapis.com/v0/b/apis-varias-mias.appspot.com/o/pizzeria%2Fpizza-1239077_640_com.jpg?alt=media&token=e7cde87a-08d5-4040-ac54-90f6c31eb3e3"
-    }
+    },
+    {
+      id: "4",
+      name: "Salame",
+      price: 5950,
+      ingredients: ["mozzarella", "salame", "orégano"],
+      img: "https://plus.unsplash.com/premium_photo-1667682942148-a0c98d1d70db?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      },
+      {
+        id: "5",
+        name: "Cuatro Quesos",
+        price: 7950,
+        ingredients: ["mozzarella", "gorgonzola", "parmesano", "queso de cabra"],
+        img: "https://plus.unsplash.com/premium_photo-1667682940111-8b3159089aa1?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
+      },
+      {
+        id: "6",
+        name: "Vegetariana",
+        price: 6950,
+        ingredients: ["mozzarella", "pimientos", "espinaca", "cebolla", "aceitunas"],
+        img: "https://images.unsplash.com/photo-1617343251257-b5d709934ddd?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      }
   ];
